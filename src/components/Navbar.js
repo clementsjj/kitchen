44a6ab242208325bf356e892cbd472eeb5fcd6ec
unknown <clementsjj@gmail.com>
2018-09-20 02:34:45 -0400

    add card to ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,12 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
+const primary = "#512da8";
+
 const Navbar = () => {
   return (
     <div>
-      <AppBar position="static">
+      <AppBar position="static" inkBarStyle={{ background: "purple" }}>
         <Toolbar>
           <Typography variant="title" color="inherit">
             The Kitchen
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import Grid from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import Cardy from "./Cardy";
 
 class Search extends Component {
   onSearchInputChange = event => {
@@ -16,6 +18,15 @@ class Search extends Component {
           margin="normal"
           onChange={this.onSearchInputChange}
         />
+        <br />
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+        <Grid container spacing={24} style={{ padding: 24 }}>
+          <Grid iem xs={12} sm={6} lg={4} xl={3}>
+            <Cardy />
+          </Grid>
+        </Grid>
       </div>
     );
   }
